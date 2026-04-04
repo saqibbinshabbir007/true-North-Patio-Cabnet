d27596526cddfde22738a40d45149de776a9e8a6
Saqib Bin Shabbir <saqibbinshabbir@gmail.com>
2026-04-05 00:31:31 +0500

    Remove Lovable references

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import { galleryPlugin } from "./vite-plugin-gallery";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
-    galleryPlugin(),
-    mode === "development" && componentTagger(),
+    galleryPlugin()
   ].filter(Boolean),
   server: {
     host: "::",
